e8b928c0e34f4e3c4a122211a9e644698b91205b
Kefilwe Magasa <kefi.magasa@gmail.com>
2024-06-27 11:32:01 -0400

    images

--- my-react-app/src/components/Blog/BlogHome.js
+++ my-react-app/src/components/Blog/BlogHome.js
@@ -1,9 +1,41 @@
 import React from 'react'
 
 const BlogHome = () => {
+    const data = [
+        {
+            id:1,
+            titleOne: "You can change the world",
+            titleTwo: "Strong Style",
+            paraOne: "How woemn are redirection Hollywood",
+            paraTwo: "New ways to wear denim",
+            name: "By Jasper Nugget",
+            subTitle: "How to shop midnfully",
+            qrCode: "/images/qrcode.png",
+            cover: "images/blog%.jpg",
+        },
+    ]
     return (
         <>
-        <h1> Blog </h1>
+        <section className='home'>
+            <div className='left-content'>
+                {data.map((value) => {
+                    return (
+                        <div className='content'>
+                            <div className='logo'>
+                                <h1>S</h1>
+                                </div>
+                                <div className="home-img">
+                                    <img src={value.cover} alt=""/>
+                                    </div>
+                                    
+                            </div>
+                    )
+                }
+                )}
+            </div>
+            <div className='right-content'></div>
+
+        </section>
         </>
     )
 }
